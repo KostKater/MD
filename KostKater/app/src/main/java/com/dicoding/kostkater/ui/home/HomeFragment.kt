d84c5470626bf9f4b7eb1b80e579e26ff09d74b8
melisaou <melisaangelina08@gmail.com>
2023-05-27 02:30:27 +0700

    Create XML files for recommendation

--- KostKater/app/src/main/java/com/dicoding/kostkater/ui/home/HomeFragment.kt
+++ KostKater/app/src/main/java/com/dicoding/kostkater/ui/home/HomeFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
 import com.dicoding.kostkater.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -28,10 +29,9 @@ class HomeFragment : Fragment() {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+        val layoutManager = GridLayoutManager(requireActivity(), 2)
+        binding.rvRecommendation.layoutManager = layoutManager
+
         return root
     }
 
