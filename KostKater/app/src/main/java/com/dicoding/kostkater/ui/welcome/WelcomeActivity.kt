8cfb45208a595c5d457da5a541c162455d8ca465
melisaou <melisaangelina08@gmail.com>
2023-06-07 05:55:11 +0700

    Create welcome activity

---
+++ KostKater/app/src/main/java/com/dicoding/kostkater/ui/welcome/WelcomeActivity.kt
@@ -0,0 +1,47 @@
+package com.dicoding.kostkater.ui.welcome
+
+import android.content.Intent
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
+import com.dicoding.kostkater.R
+import com.dicoding.kostkater.databinding.ActivityWelcomeBinding
+import com.dicoding.kostkater.ui.login.LoginActivity
+
+class WelcomeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityWelcomeBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityWelcomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupView()
+        setupAction()
+    }
+
+    private fun setupView() {
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+        supportActionBar?.hide()
+    }
+
+    private fun setupAction() {
+        binding.loginButton.setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
+
+        binding.registerButton.setOnClickListener {
+//            startActivity(Intent(this, RegisterActivity::class.java))
+        }
+    }
+}
