cc6d1286988a5dc36dc1e56eef3505c9caf78022
melisaou <melisaangelina08@gmail.com>
2023-06-07 18:43:48 +0700

    Revamp home page UI

--- KostKater/app/src/main/java/com/dicoding/kostkater/ui/main/MainActivity.kt
+++ KostKater/app/src/main/java/com/dicoding/kostkater/ui/main/MainActivity.kt
@@ -1,42 +1,84 @@
 package com.dicoding.kostkater.ui.main
 
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.text.Html
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.view.WindowInsets
+import android.view.WindowManager
+import androidx.appcompat.app.ActionBar
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.dicoding.kostkater.R
+import com.dicoding.kostkater.adapter.RecommendationAdapter
 import com.dicoding.kostkater.databinding.ActivityMainBinding
+import com.dicoding.kostkater.model.Meal
+import com.dicoding.kostkater.ui.dialog.FilterSheet
+import com.dicoding.kostkater.ui.dialog.PreferenceSheet
+import com.dicoding.kostkater.ui.home.HomeViewModel
 import com.dicoding.kostkater.ui.login.LoginActivity
 import com.dicoding.kostkater.ui.welcome.WelcomeActivity
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var mainViewModel: MainViewModel
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navView: BottomNavigationView = binding.navView
+        setupViewModel()
 
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home, R.id.navigation_meal_plan
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+        val layoutManager = GridLayoutManager(this, 2)
+        binding.rvRecommendation.layoutManager = layoutManager
+
+        binding.preferenceButton.setOnClickListener {
+            PreferenceSheet().show(supportFragmentManager, "preferenceTag")
+        }
+
+        binding.budgetButton.setOnClickListener {
+            FilterSheet().show(supportFragmentManager, "budgetTag")
+        }
+    }
+
+    private fun setupViewModel() {
+        mainViewModel = ViewModelProvider(this)[MainViewModel::class.java]
+
+        mainViewModel.meals.observe(this) { recommendations ->
+            setRecommendationData(recommendations)
+        }
+
+        mainViewModel.isLoading.observe(this) {
+            showLoading(it)
+        }
+    }
+
+    private fun setRecommendationData(meals: List<Meal>) {
+        val adapter = RecommendationAdapter(meals)
+        binding.rvRecommendation.adapter = adapter
+    }
+
+    private fun showLoading(isLoading: Boolean) {
+        if (isLoading) {
+            binding.progressBar.visibility = View.VISIBLE
+        } else {
+            binding.progressBar.visibility = View.GONE
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
