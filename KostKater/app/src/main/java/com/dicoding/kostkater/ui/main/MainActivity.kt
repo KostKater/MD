954181318dcd3e3813f409f7d4cd0694da10404b
melisaou <melisaangelina08@gmail.com>
2023-06-10 18:47:14 +0700

    Add greeting to banner

--- KostKater/app/src/main/java/com/dicoding/kostkater/ui/main/MainActivity.kt
+++ KostKater/app/src/main/java/com/dicoding/kostkater/ui/main/MainActivity.kt
@@ -2,7 +2,7 @@ package com.dicoding.kostkater.ui.main
 
 import android.content.Context
 import android.content.Intent
-import android.graphics.drawable.ColorDrawable
+import android.graphics.Color
 import android.os.Bundle
 import android.text.Spannable
 import android.text.SpannableString
@@ -10,8 +10,10 @@ import android.text.style.ForegroundColorSpan
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.startActivity
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.preferencesDataStore
@@ -26,6 +28,7 @@ import com.dicoding.kostkater.ui.ViewModelFactory
 import com.dicoding.kostkater.ui.dialog.FilterSheet
 import com.dicoding.kostkater.ui.dialog.PreferenceSheet
 import com.dicoding.kostkater.ui.welcome.WelcomeActivity
+import java.util.Calendar
 
 private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
 
@@ -39,6 +42,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        setGreeting()
         setupViewModel()
 
         val layoutManager = GridLayoutManager(this, 2)
@@ -101,11 +105,51 @@ class MainActivity : AppCompatActivity() {
             }
 
             R.id.action_logout -> {
-                mainViewModel.logout()
+                AlertDialog.Builder(this).apply {
+                    setTitle(getString(R.string.logout_confirm))
+                    setMessage(getString(R.string.logout_confirm_desc))
+                    setPositiveButton(getString(R.string.logout)) { _, _ ->
+                        mainViewModel.logout()
+                    }
+                    setNegativeButton(getString(R.string.cancel)) { dialog, _ ->
+                        dialog.dismiss()
+                    }
+                    create()
+                    show()
+                }
                 true
             }
 
             else -> true
         }
+    }
+
+    private fun getGreeting(): String {
+        val calendar = Calendar.getInstance()
+
+        return when (calendar.get(Calendar.HOUR_OF_DAY)) {
+            in 5..9 -> "pagi"
+            in 10..14 -> "siang"
+            in 15..17 -> "sore"
+            else -> "malam"
+        }
+    }
+
+    private fun setGreeting() {
+        val greeting = "Selamat"
+        val timeOfDay = getGreeting()
+        val text = "$greeting $timeOfDay."
+        val spannableString = SpannableString(text)
+
+        val whiteColorSpan = ForegroundColorSpan(ContextCompat.getColor(this, R.color.white))
+        spannableString.setSpan(whiteColorSpan, 0, greeting.length, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+        val colorSpan = ForegroundColorSpan(ContextCompat.getColor(this, R.color.selective_yellow))
+
+        val startIndex = greeting.length
+        val endIndex = startIndex + timeOfDay.length + 2
+        spannableString.setSpan(colorSpan, startIndex, endIndex, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+        binding.tvGreetings.text = spannableString
     }
 }
